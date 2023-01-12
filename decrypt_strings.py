22dc7ead608d4169284d02320871c74d2d6350fd
735859767163224085 <nemerux@gmail.com>
2023-01-12 22:54:10 +0100

    more decrypted strings
    should use every function instead but idk how

--- decrypt_strings.py
+++ decrypt_strings.py
@@ -2,13 +2,28 @@ import angr
 import time
 
 proj = angr.Project("sus", auto_load_libs=False)
-decrypt = next(filter(lambda x: x.name.startswith("3Qa"), proj.loader.symbols))
-state = proj.factory.call_state(decrypt.rebased_addr)
 
-while True:
-  state = state.step().successors[0]
-  try:
-    block = state.block()
-  except:
-    break
-print(state.memory.hex_dump(0x7763b0 - 1000, 2000))
+symbols = []
+for decrypt in proj.loader.symbols:
+  if len(decrypt.name) < 18:
+    continue
+  if decrypt.rebased_addr in symbols:
+    continue
+  symbols.append(decrypt.rebased_addr)
+  mnemonic = proj.factory.block(decrypt.rebased_addr).disassembly.insns[0].mnemonic
+  op_str = proj.factory.block(decrypt.rebased_addr).disassembly.insns[0].op_str
+  if (mnemonic == "mov" and "al, byte ptr" in op_str) or mnemonic == "movaps":
+    symbols.append(decrypt.rebased_addr)
+    state = proj.factory.call_state(decrypt.rebased_addr)
+
+    data_addr = int(op_str.split("0x")[1][:-1],16)
+    rip1 = state.regs.rip.args[0]
+    
+    while True:
+      state = state.step().successors[0]
+      try:
+        block = state.block()
+      except:
+        break
+
+    print(state.memory.hex_dump(rip1 + data_addr, 2000))
